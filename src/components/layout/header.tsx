6b36bf105f28fee4e6d09b432f721ac3faffda5c
Tchakoumi <lorraintchakoumi@gmail.com>
2025-06-11 06:43:53 +0200

    refactor(Header): Clean up layout structure and enhance button functionality for registration

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,67 +1,77 @@
 'use client';
-import { Box, Button, Toolbar, Typography } from "@mui/material";
-import { LanguageSwapper } from "@xafpay/shared";
-import { INavItem } from "@xafpay/types";
-import Image from "next/image";
-import { NavItem } from "./NavItem";
-import { useIntl } from "react-intl";
+import { Box, Button, Toolbar, Typography } from '@mui/material';
+import { LanguageSwapper } from '@xafpay/shared';
+import { INavItem } from '@xafpay/types';
+import Image from 'next/image';
+import { NavItem } from './NavItem';
+import { useIntl } from 'react-intl';
 
 export default function Header() {
-    const { formatMessage } = useIntl();
+  const { formatMessage } = useIntl();
 
-
-    const navItems: INavItem[] = [
-        { label: formatMessage({ id: 'howItWorksTitle' }), href: '#howItWorks' },
-        { label: 'FAQ', href: '#faq' },
-    ];
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            height: '128px',
-            padding: '24px 117.5px',
-        }}>
-            <Toolbar sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                width: '100%',
-                height: '100%',
-            }}>
-                <Image
-                    src="/assets/logo.png"
-                    alt="Xafpay Logo"
-                    width={166}
-                    height={80}
-                />
-                <Box
-                    sx={{
-                        display: 'grid',
-                        columnGap: 10,
-                        gridAutoFlow: 'column'
-                    }}
-                >
-                    {navItems.map((item, index) => (
-                        <NavItem key={index} navItem={item} />
-                    ))}
-                </Box>
-                <Box sx={{
-                    display: 'grid',
-                    gridAutoFlow: 'column',
-                    columnGap: 2,
-                    alignItems: 'center',
-                }}>
-                    <LanguageSwapper />
-                    <Button
-                        variant='contained'
-                        onClick={() => alert('Login clicked')}
-                    >
-                        <Typography variant="p2r" fontFamily='Poppins'>
-                            {formatMessage({ id: 'register' })}
-                        </Typography>
-                    </Button>
-                </Box>
-            </Toolbar>
+  const navItems: INavItem[] = [
+    { label: formatMessage({ id: 'howItWorksTitle' }), href: '#howItWorks' },
+    { label: 'FAQ', href: '#faq' },
+  ];
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        height: '128px',
+        padding: '24px 117.5px',
+      }}
+    >
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Image
+          src="/assets/logo.png"
+          alt="Xafpay Logo"
+          width={166}
+          height={80}
+        />
+        <Box
+          sx={{
+            display: 'grid',
+            columnGap: 10,
+            gridAutoFlow: 'column',
+          }}
+        >
+          {navItems.map((item, index) => (
+            <NavItem key={index} navItem={item} />
+          ))}
         </Box>
-    )
+        <Box
+          sx={{
+            display: 'grid',
+            gridAutoFlow: 'column',
+            columnGap: 2,
+            alignItems: 'center',
+          }}
+        >
+          <LanguageSwapper />
+          <Button
+            variant="contained"
+            onClick={() =>
+              window.open(
+                `${process.env.NEXT_PUBLIC_APP_URL}/register`,
+                '_blank'
+              )
+            }
+          >
+            <Typography variant="p2r" fontFamily="Poppins">
+              {formatMessage({ id: 'register' })}
+            </Typography>
+          </Button>
+        </Box>
+      </Toolbar>
+    </Box>
+  );
 }
