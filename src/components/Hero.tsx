b4b0a8169f3883965d81c91913a9ea21d3aa715e
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-06-12 19:18:09 +0100

    refactor(app): #3 Improve layout and styling of Hero component for better user experience

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -203,10 +203,8 @@ export default function Hero() {
           <Box
             sx={{
               display: 'grid',
-              rowGap: 2,
               border: '1px solid darkgray',
               borderRadius: 3,
-              padding: 2,
               boxShadow:
                 '-40px -40px 8px rgba(21, 124, 251, 0.3), 0px 7px 8px rgba(251, 1, 2, 0.15)',
               maxWidth: '25rem',
@@ -218,253 +216,278 @@ export default function Hero() {
               backgroundColor: theme.palette.background.paper,
             }}
           >
-            <Typography
-              variant="h4"
-              sx={{
-                textAlign: 'center',
-                fontFamily: 'Space Grotesk',
-                fontWeight: 600,
-                color: theme.palette.primary.main,
-                lineHeight: '130%',
-              }}
-            >
-              {formatMessage({ id: 'todayRate' })}: {activeCurrency ?
-                `1 ${activeCurrency.currency} = XAF ${activeCurrency.xaf_rate.toFixed(2)}`
-                :
-                'loading...'
-              }
-            </Typography>
-            <Divider />
-            <Box
-              sx={{
-                display: 'grid',
-                textAlign: 'start',
-              }}
-            >
+            <Box sx={{
+              display: 'grid',
+              rowGap: 0.5,
+              backgroundColor: theme.palette.primary.main,
+              borderRadius: '12px 12px 0 0',
+              padding: 1,
+              color: 'white'
+            }}>
               <Typography
-                variant="p3r"
+                variant="h4"
                 sx={{
-                  fontSize: { mobile: '10px', tablet: '12px' },
-                  lineHeight: '160%',
-                  color: '#12192C',
-                  fontFamily: 'Poppins',
+                  textAlign: 'center',
+                  fontFamily: 'Space Grotesk',
+                  fontWeight: 600,
+                  lineHeight: '130%',
                 }}
               >
-                {formatMessage({ id: 'whenYouSend' })}
+                {activeCurrency ?
+                  `1 ${activeCurrency.currency} = XAF ${activeCurrency.xaf_rate.toFixed(2)}`
+                  :
+                  'loading...'
+                }
               </Typography>
-              <Box
+              <Typography
+                variant="h4"
                 sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
+                  textAlign: 'center',
+                  fontFamily: 'Space Grotesk',
+                  fontWeight: 600,
+                  lineHeight: '130%',
                 }}
               >
-                <TextField
-                  variant="standard"
-                  type="number"
-                  placeholder="100 $"
-                  sx={{
-                    '& .MuiInputBase-input': {
-                      textAlign: 'start',
-                      fontSize: { tablet: '36px', mobile: '24px' },
-                      fontWeight: '700',
-                      fontFamily: 'Poppins',
-                      width: '15rem',
-                    },
-                    '& .MuiInput-root::before': {
-                      border: 'none',
-                    },
-                  }}
-                  InputProps={{
-                    disableUnderline: true,
-                  }}
-                  onChange={handleChange}
-                  value={amount}
-                  disabled={false}
-                />
-                <Box
-                  sx={{
-                    display: 'grid',
-                    gridAutoFlow: 'column',
-                    columnGap: 1,
-                    alignItems: 'center',
-                    cursor: 'pointer',
-                  }}
-                  onClick={(e) => setCurrencyAnchorEl(e.currentTarget)}
-                >
-                  {activeCurrency && (
-                    <Image
-                      src={correspondingFlags[activeCurrency.currency]}
-                      width={24}
-                      height={24}
-                      alt={`${activeCurrency} flag`}
-                    />
-                  )}
-                  <Typography
-                    variant="p1m"
-                    sx={{
-                      fontFamily: 'Space Grotesk',
-                      fontSize: { tablet: '24px', mobile: '14px' },
-                      lineHeight: '130%',
-                    }}
-                  >
-                    {activeCurrency?.currency}
-                  </Typography>
-                  <Icon icon={down} />
-                </Box>
-              </Box>
+                {formatMessage({ id: 'feesRates' })}
+              </Typography>
             </Box>
-            <Divider
-              variant="fullWidth"
-              sx={{
-                backgroundColor: 'transparent',
-                borderStyle: 'dashed',
-              }}
-            >
-              <Chip
-                label={<Icon icon={arrowDownwardIcon} />}
-                sx={{
-                  backgroundColor: '#0E103A',
-                  color: 'white',
-                  fontSize: '1.2rem',
-                  height: '36px',
-                  width: '36px',
-                  '& .MuiChip-label': {
-                    padding: 0,
-                  },
-                }}
-              />
-            </Divider>
-            <Box
-              sx={{
-                display: 'grid',
-                textAlign: 'start',
-              }}
-            >
-              <Typography
-                variant="p3r"
-                sx={{
-                  fontSize: { mobile: '10px', tablet: '12px' },
-                  lineHeight: '160%',
-                  color: '#12192C',
-                  fontFamily: 'Poppins',
-                }}
-              >
-                {formatMessage({ id: 'WillReceive' })}
-              </Typography>
+            <Box sx={{
+              display: 'grid',
+              rowGap: 2,
+              padding: '0 16px 16px'
+            }}>
+              <Divider />
               <Box
                 sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
+                  display: 'grid',
+                  textAlign: 'start',
                 }}
               >
+                <Typography
+                  variant="p3r"
+                  sx={{
+                    fontSize: { mobile: '10px', tablet: '12px' },
+                    lineHeight: '160%',
+                    color: '#12192C',
+                    fontFamily: 'Poppins',
+                  }}
+                >
+                  {formatMessage({ id: 'whenYouSend' })}
+                </Typography>
                 <Box
                   sx={{
                     display: 'flex',
-                    alignItems: 'end',
-                    gap: 1,
-                    flexGrow: 1,
-                  }}
-                >
-                  <Typography
-                    variant="h3"
-                    sx={{
-                      textAlign: 'start',
-                      fontSize: { tablet: '36px', mobile: '24px' },
-                      fontWeight: '700',
-                      fontFamily: 'Poppins',
-                    }}
-                  >
-                    {amount && activeCurrency
-                      ? formatNumber(
-                        Number((Number(amount) * activeCurrency.xaf_rate).toFixed(2))
-                      )
-                      : '...'}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: 'grid',
-                    gridAutoFlow: 'column',
-                    columnGap: 1,
                     alignItems: 'center',
+                    justifyContent: 'space-between',
                   }}
                 >
-                  <Image
-                    src="/assets/cameroon-flag.png"
-                    width={24}
-                    height={24}
-                    alt="cameroon flag"
-                  />
-                  <Typography
-                    variant="p1m"
+                  <TextField
+                    variant="standard"
+                    type="number"
+                    placeholder="100 $"
                     sx={{
-                      fontFamily: 'Space Grotesk',
-                      fontWeight: 500,
-                      fontSize: { tablet: '24px', mobile: '14px' },
-                      lineHeight: '130%',
+                      '& .MuiInputBase-input': {
+                        textAlign: 'start',
+                        fontSize: { tablet: '36px', mobile: '24px' },
+                        fontWeight: '700',
+                        fontFamily: 'Poppins',
+                        width: '15rem',
+                      },
+                      '& .MuiInput-root::before': {
+                        border: 'none',
+                      },
                     }}
+                    InputProps={{
+                      disableUnderline: true,
+                    }}
+                    onChange={handleChange}
+                    value={amount}
+                    disabled={false}
+                  />
+                  <Box
+                    sx={{
+                      display: 'grid',
+                      gridAutoFlow: 'column',
+                      columnGap: 1,
+                      alignItems: 'center',
+                      cursor: 'pointer',
+                    }}
+                    onClick={(e) => setCurrencyAnchorEl(e.currentTarget)}
                   >
-                    XAF
-                  </Typography>
+                    {activeCurrency && (
+                      <Image
+                        src={correspondingFlags[activeCurrency.currency]}
+                        width={24}
+                        height={24}
+                        alt={`${activeCurrency} flag`}
+                      />
+                    )}
+                    <Typography
+                      variant="p1m"
+                      sx={{
+                        fontFamily: 'Space Grotesk',
+                        fontSize: { tablet: '24px', mobile: '14px' },
+                        lineHeight: '130%',
+                      }}
+                    >
+                      {activeCurrency?.currency}
+                    </Typography>
+                    <Icon icon={down} />
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-            <Divider />
-            <Box
-              sx={{
-                display: 'grid',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                gridAutoFlow: 'column',
-              }}
-            >
-              <Typography
-                variant="p2m"
+              <Divider
+                variant="fullWidth"
                 sx={{
-                  fontSize: { mobile: '10px', tablet: '12px' },
-                  lineHeight: '160%',
-                  fontFamily: 'Poppins',
+                  backgroundColor: 'transparent',
+                  borderStyle: 'dashed',
                 }}
               >
-                {formatMessage({ id: 'OurTransactionFees' })}
-              </Typography>
-              <Typography
-                variant="p2m"
+                <Chip
+                  label={<Icon icon={arrowDownwardIcon} />}
+                  sx={{
+                    backgroundColor: '#0E103A',
+                    color: 'white',
+                    fontSize: '1.2rem',
+                    height: '36px',
+                    width: '36px',
+                    '& .MuiChip-label': {
+                      padding: 0,
+                    },
+                  }}
+                />
+              </Divider>
+              <Box
                 sx={{
-                  fontSize: { mobile: '10px', tablet: '12px' },
-                  lineHeight: '160%',
-                  fontFamily: 'Poppins',
-                  color: theme.palette.primary.dark,
+                  display: 'grid',
+                  textAlign: 'start',
+                }}
+              >
+                <Typography
+                  variant="p3r"
+                  sx={{
+                    fontSize: { mobile: '10px', tablet: '12px' },
+                    lineHeight: '160%',
+                    color: '#12192C',
+                    fontFamily: 'Poppins',
+                  }}
+                >
+                  {formatMessage({ id: 'WillReceive' })}
+                </Typography>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'end',
+                      gap: 1,
+                      flexGrow: 1,
+                    }}
+                  >
+                    <Typography
+                      variant="h3"
+                      sx={{
+                        textAlign: 'start',
+                        fontSize: { tablet: '36px', mobile: '24px' },
+                        fontWeight: '700',
+                        fontFamily: 'Poppins',
+                      }}
+                    >
+                      {amount && activeCurrency
+                        ? formatNumber(
+                          Number((Number(amount) * activeCurrency.xaf_rate).toFixed(2))
+                        )
+                        : '...'}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: 'grid',
+                      gridAutoFlow: 'column',
+                      columnGap: 1,
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Image
+                      src="/assets/cameroon-flag.png"
+                      width={24}
+                      height={24}
+                      alt="cameroon flag"
+                    />
+                    <Typography
+                      variant="p1m"
+                      sx={{
+                        fontFamily: 'Space Grotesk',
+                        fontWeight: 500,
+                        fontSize: { tablet: '24px', mobile: '14px' },
+                        lineHeight: '130%',
+                      }}
+                    >
+                      XAF
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Divider />
+              <Box
+                sx={{
+                  display: 'grid',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  gridAutoFlow: 'column',
+                }}
+              >
+                <Typography
+                  variant="p2m"
+                  sx={{
+                    fontSize: { mobile: '10px', tablet: '12px' },
+                    lineHeight: '160%',
+                    fontFamily: 'Poppins',
+                  }}
+                >
+                  {formatMessage({ id: 'OurTransactionFees' })}
+                </Typography>
+                <Typography
+                  variant="p2m"
+                  sx={{
+                    fontSize: { mobile: '10px', tablet: '12px' },
+                    lineHeight: '160%',
+                    fontFamily: 'Poppins',
+                    color: theme.palette.primary.dark,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {formatMessage({ id: 'feesRate' })}
+                </Typography>
+              </Box>
+              <Divider />
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                endIcon={<Icon icon={arrowRight} />}
+                sx={{
+                  fontSize: '1.2rem',
                   fontWeight: 'bold',
+                  fontFamily: 'Poppins',
+                  width: '100%',
                 }}
+                onClick={() =>
+                  window.open(
+                    `${process.env.NEXT_PUBLIC_APP_URL}/amount=${amount}&currency=${activeCurrency?.currency}`,
+                    '_blank'
+                  )
+                }
+                disabled={false || !activeCurrency}
               >
-                {formatMessage({ id: 'feesRate' })}
-              </Typography>
+                {formatMessage({ id: 'transferNow' })}
+              </Button>
             </Box>
-            <Divider />
-            <Button
-              size="large"
-              variant="contained"
-              color="primary"
-              endIcon={<Icon icon={arrowRight} />}
-              sx={{
-                fontSize: '1.2rem',
-                fontWeight: 'bold',
-                fontFamily: 'Poppins',
-                width: '100%',
-              }}
-              onClick={() =>
-                window.open(
-                  `${process.env.NEXT_PUBLIC_APP_URL}/amount=${amount}&currency=${activeCurrency?.currency}`,
-                  '_blank'
-                )
-              }
-              disabled={false || !activeCurrency}
-            >
-              {formatMessage({ id: 'transferNow' })}
-            </Button>
           </Box>
         </Box>
       </Box>
